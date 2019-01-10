b450450b4f13f64a7eac90dc9f4e1830061002de
AXsmasher <keeswtucker@gmail.com>
2019-01-11 11:45:28 +1300

    Moved Chunks so they work in terrain
    Started terrain gen.

---
+++ afunroyalegame_main/Assets/GenerateTerrain.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateTerrain : MonoBehaviour {
+
+    public GameObject[] chunks;
+
+    public float currentPosition = 0;
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < chunks.Length; i++)
+        {
+            currentPosition -= chunks[i].GetComponent<ChunkData>().width;
+        }
+        currentPosition = currentPosition / 2;
+        for (int i = 0; i < chunks.Length; i++)
+        {
+            GameObject chunk = Instantiate(chunks[i], new Vector3(currentPosition, 0, 0), Quaternion.identity);
+            currentPosition += chunk.GetComponent<ChunkData>().width;
+            chunk.transform.parent = transform;
+        }
+	}
+}
