c7eb16cebed08ff320c6156ec1ff15c722ae11ff
AXsmasher <keeswtucker@gmail.com>
2019-02-22 18:29:39 +1300

    One last thing...

--- afunroyalegame_main/Assets/DeleteDisclaimer.cs
+++ afunroyalegame_main/Assets/DeleteDisclaimer.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class DeleteDisclaimer : MonoBehaviour {
 
 	// Update is called once per frame
+    void Start()
+    {
+        GameObject.Find("LoadingPlayer").GetComponent<Rigidbody>().isKinematic = true;
+    }
+
 	void Update () {
         if (Input.GetKey("space"))
         {
             gameObject.SetActive(false);
+            GameObject.Find("LoadingPlayer").GetComponent<Rigidbody>().isKinematic = false;
         }
 	}
 }
